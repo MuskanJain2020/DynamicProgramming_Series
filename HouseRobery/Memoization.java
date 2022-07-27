24f9ea63d3a22ad7cacf2f845077139d32b16b8c
Muskan Jain <83420072+MuskanJain2020@users.noreply.github.com>
2022-07-27 20:22:38 +0530

    Create Memoization.java

---
+++ HouseRobery/Memoization.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int rob(int[] nums) {
+        
+        int k = nums.length ;
+        int dp[] = new int [k+1] ;
+        for (int i=0 ; i<=k ; i++){
+            dp[i] = -1 ;
+        }
+        int a =Solve(nums , 1 , k , dp) ;
+        int b = Solve(nums , 0 , k ,dp ) ;
+        
+        System.out.println(a + " "+ b);
+        
+            return Math.max(a,b) ;
+         
+      }
+    
+    public static int  Solve (int []nums , int i , int n , int[]dp){
+        if(i==n){
+            return 0 ;
+        }
+        if(i == n-1){
+            return nums[n-1] ;
+            }
+        
+        if(dp[i] !=-1){
+            return dp[i] ;
+        }
+        
+        int inc = nums[i] +Solve (nums , i+2 , n ,dp) ;
+        int ninc = Solve(nums , i+1 , n ,dp) ;
+        dp[i] = Math.max(inc , ninc) ;
+        
+        return dp[i] ;
+    }
+    
+    
+    
+    
+    
+    
+}

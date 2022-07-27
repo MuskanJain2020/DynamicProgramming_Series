5c3b7d931ad4acff424394da442d6799c14f7fd2
Muskan Jain <83420072+MuskanJain2020@users.noreply.github.com>
2022-07-27 17:09:02 +0530

    minimum elements (Plain Recursion)

--- Minimum Elements/Recursion.java
+++ Minimum Elements/Recursion.java
@@ -1,1 +1,44 @@
-# DynamicProgramming_Series
+public class MinimumElements {
+    public static void main(String []args){
+        int a[] = {1,2,3};
+              calculate(a,11) ;
+        System.out.println(
+                calculate(a,4)
+        );
+    }
+
+
+    static int calculate(int num[] , int x ){
+       Plain Recursion
+     int ans  =  solve(num , x);
+     if(ans == Integer.MAX_VALUE){
+         return -1 ;
+     }
+
+    return  ans ;
+
+  }
+
+ 
+    public static int solve(int num[] , int x , int[]dp){
+        if(x==0){
+            return 0 ;
+        }
+        if(x<0){
+            return Integer.MAX_VALUE ;
+        }
+
+       
+        int mini = Integer.MAX_VALUE ;
+        for(int i =0 ; i<num.length ; i++){
+            int ans = solve(num , x-num[i] ,dp);
+            if(ans != Integer.MAX_VALUE ){
+                mini =Math.min(mini, 1+ans);
+            }
+           
+
+        }
+        return mini ;
+    }
+  }
+
